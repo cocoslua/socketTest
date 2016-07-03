8ab7f8c78c634177add14cdc2b50eb403fe055e9
cocoslua <liuxingfeng@liuxingfengdeMac-mini.local>
2016-07-03 16:25:58 +0800

    manager
    添加了 管理类

---
+++ ASSocketManager.h
@@ -0,0 +1,13 @@
+//
+//  ASSocketManager.h
+//  socketTest
+//
+//  Created by 刘兴丰 on 16/7/3.
+//  Copyright © 2016年 刘兴丰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASSocketManager : NSObject
+
+@end
---
+++ ASSocketManager.m
@@ -0,0 +1,13 @@
+//
+//  ASSocketManager.m
+//  socketTest
+//
+//  Created by 刘兴丰 on 16/7/3.
+//  Copyright © 2016年 刘兴丰. All rights reserved.
+//
+
+#import "ASSocketManager.h"
+
+@implementation ASSocketManager
+
+@end
